fb0c90fa07dc1e877c10e921690aab51f628cafd
Leo Generali <leogen17@gmail.com>
2017-02-09 22:27:41 -0500

    Add click functionality to arrow keys

--- js/key.js
+++ js/key.js
@@ -1,8 +1,8 @@
 
 key = {
-
   init: function(){
     window.addEventListener('keydown', this.pressButton);
+    window.addEventListener('click', this.clickButton);
     this.apply();
   },
 
@@ -10,7 +10,6 @@ key = {
 
   //Removes the pressed effect from each arrow
   removeTransition: function(e){
-    console.log(e);
     e.target.classList.remove('pressed');
   },
 
@@ -18,11 +17,17 @@ key = {
   pressButton: function(e){
     const btn = document.querySelector(`div[data-key="${e.keyCode}"]`);
     btn.classList.add('pressed');
+    console.log(e.keyCode);
   },
+
+  clickButton: function(e){
+    if(e.target.tagName !== "HTML"){
+      e.target.classList.add('pressed');
+    }
+  },
+
 
   apply: function(){
     this.buttons.forEach(button => button.addEventListener('transitionend', key.removeTransition));
   }
 }
-
-key.init();
