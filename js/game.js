87c56396d6d040ed0c0ba20bd1c3cfe78b128ae5
Leo Generali <leogen17@gmail.com>
2017-03-06 00:19:55 -0500

    Adds start button

--- js/game.js
+++ js/game.js
@@ -1,2 +1,20 @@
-key.init();
-ai.selectNewKey();
+game = {
+  init: function(){
+    key.init();
+    ai.selectNewKey();
+  },
+
+  newInit: function(){
+    var startButton = document.getElementById('start-btn');
+    
+    key.init();
+    ai.selectNewKey();
+  }
+
+  gameStart: function(){
+    var startButton = document.getElementById('start-btn');
+    startButton.addEventListener('click', this.init);
+  }
+}
+
+game.gameStart();
